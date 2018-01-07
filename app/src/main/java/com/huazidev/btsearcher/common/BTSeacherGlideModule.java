37e69beb463cb6cf92086714c60a59db9e085e0f
huazidev <huazi.dev@gmail.com>
2018-01-07 22:11:18 +0800

    feat: upgrade some library, add glide library

---
+++ app/src/main/java/com/huazidev/btsearcher/common/BTSeacherGlideModule.java
@@ -0,0 +1,13 @@
+package com.huazidev.btsearcher.common;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * @author hua on 2018/1/7.
+ */
+@GlideModule
+public final class BTSeacherGlideModule extends AppGlideModule {
+
+
+}
---
+++ app/src/main/java/com/huazidev/btsearcher/common/GlideHepler.java
@@ -0,0 +1,40 @@
+package com.huazidev.btsearcher.common;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+/**
+ * @author hua on 2018/1/7.
+ */
+public final class GlideHepler {
+
+    // 避免activity被销毁后，避免出现异常
+    public static GlideRequests with(Context context) {
+        if (context instanceof Activity && ((Activity) context).isDestroyed()) {
+            return GlideApp.with(context.getApplicationContext());
+        }
+        return GlideApp.with(context);
+    }
+
+    public static GlideRequest<Drawable> request(ImageView target, Object model, @DrawableRes int placeholder) {
+        return with(target.getContext())
+                .load(model)
+                .placeholder(placeholder)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .centerCrop();
+    }
+
+    public static void loadImage(ImageView target, Object model) {
+        loadImage(target, model, 0);
+    }
+
+    public static void loadImage(ImageView target, Object model, @DrawableRes int placeholder) {
+        request(target, model, placeholder);
+    }
+
+}
